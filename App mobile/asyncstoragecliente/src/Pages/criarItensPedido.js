6edc97640ddc60939afc19bd4c0471254fb9c764
Giovanni <gvi_lf@hotmail.com>
2024-04-02 17:25:22 -0300

    0204

--- App mobile/asyncstoragecliente/src/Pages/criarItensPedido.js
+++ App mobile/asyncstoragecliente/src/Pages/criarItensPedido.js
@@ -8,30 +8,43 @@ import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 
 export default function CriarItensPedido() {
-    
+
     const [pedidoId, setPedidoId] = useState('')
     const [user, setUser] = useState('')
+    const [categorias, setCategorias] = useState([''])
 
+    useEffect(() => {
+        async function getPedidoId() {
+            const idp = await AsyncStorage.getItem('@idpedido')
+            const pedidoId = JSON.parse(idp)
+            setPedidoId(pedidoId)
+        }
+        getPedidoId()
+    }, [])
 
-  useEffect(() => {
-    async function getPedidoId(){
-        const idp = await AsyncStorage.getItem('@idpedido')
-        const pedidoId = JSON.parse(idp)
-        setPedidoId(pedidoId)
-    }
-    getPedidoId()
-  }, [])
+    useEffect(() => {
+        async function handleName() {
+            const iNome = await AsyncStorage.getItem('@nome')
+            const nome = JSON.parse(iNome)
+            setUser(nome)
+            //    console.log(nome)
+        }
+        handleName()
+    })
 
-  useEffect(() => {
-    async function handleName() {
-        const iNome = await AsyncStorage.getItem('@nome')
-        const nome = JSON.parse(iNome)
-        setUser(nome)
-        //    console.log(nome)
-    }
-    handleName()
-})
+    useEffect(() => {
 
+        async function LerCategorias() {
+            try {
+                const resposta = await apiLocal.get('/ListarCategorias')
+                setCategorias(resposta.data)
+                console.log('categorias', categorias)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        LerCategorias()
+    }, [])
 
 
 
